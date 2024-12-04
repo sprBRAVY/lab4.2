6ee205317c49f7895315ea1a079a6f4fdbe99ef6
sprBRAVY <sprbravy123456@gmail.com>
2024-12-04 19:45:43 +0300

    Update ArrayUtils.java(Рефакторинг класса ArrayUtils)

--- lab3/src/ArrayUtils.java
+++ lab3/src/ArrayUtils.java
@@ -1,12 +1,27 @@
-// ArrayUtils.java
+/**
+ * Утилитарный класс для работы с массивами.
+ */
 public class ArrayUtils {
-    // Метод для проверки, отсортирован ли массив
+
+    // Конструктор по умолчанию
+    public ArrayUtils() {
+    }
+
+    /**
+     * Проверяет, отсортирован ли массив по возрастанию.
+     *
+     * @param array массив целых чисел
+     * @return true, если массив отсортирован; иначе false
+     */
     public static boolean isSorted(int[] array) {
-        for (int i = 1; i < array.length; i++) {
+        // Проходим по массиву, начиная с 1-го элемента
+        for (int i = 1; i < array.length; ++i) {
+            // Если текущий элемент меньше предыдущего, массив не отсортирован
             if (array[i] < array[i - 1]) {
-                return false; // Массив не отсортирован
+                return false;
             }
         }
-        return true; // Массив отсортирован
+        // Если не найдено нарушений, массив отсортирован
+        return true;
     }
 }
